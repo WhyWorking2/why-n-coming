9028eae5e18131564d67fa65286ec757713d9182
jamie <northwing@naver.com>
2025-09-28 23:23:08 +0900

    [FEAT] 프로젝트 첫 세팅

---
+++ src/main/java/org/sparta/whyncoming/WhyNComingApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.whyncoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyNComingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhyNComingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/whyncoming/WhyNComingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.whyncoming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyNComingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
