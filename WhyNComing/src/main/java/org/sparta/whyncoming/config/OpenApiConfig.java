3bf964bc39a5e437e48ecfc77c14a1f5a0c40866
kookong2 <hy010309@naver.com>
2025-09-30 03:46:14 +0900

    [FEAT] DB 연결 및 테스트 작업코드 추가

---
+++ WhyNComing/src/main/java/org/sparta/whyncoming/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package org.sparta.whyncoming.config;
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String schemeName = "BearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Why-N-Coming API")
+                        .version("0.0.1")
+                        .description("스파르타 프로젝트 REST API 문서"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components().addSecuritySchemes(
+                        schemeName,
+                        new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
