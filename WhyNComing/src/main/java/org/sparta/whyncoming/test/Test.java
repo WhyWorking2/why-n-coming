06ebdb98fb461b669498d0320c8a39cf0f3f852a
jamie <northwing@naver.com>
2025-10-01 02:52:10 +0900

    [ENV] flyway 적용 #13
    의존성 추가 및 설정 추가

--- WhyNComing/src/main/java/org/sparta/whyncoming/test/Test.java
+++ WhyNComing/src/main/java/org/sparta/whyncoming/test/Test.java
@@ -8,7 +8,13 @@ import lombok.*;
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Test {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @SequenceGenerator(
+            name = "test_seq",
+            sequenceName = "test_seq",
+            allocationSize = 1        // ★ DB 시퀀스 증가폭(1)과 맞춤
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "test_seq")
     private Long id;
 
     private String name;
