3a14b76e017bba0277f62de687d4d5e459650c57
kookong2 <hy010309@naver.com>
2025-09-30 15:22:23 +0900

    [REFACTOR] 예제 코드 수정

--- WhyNComing/src/main/java/org/sparta/whyncoming/test/TestController.java
+++ WhyNComing/src/main/java/org/sparta/whyncoming/test/TestController.java
@@ -4,7 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.sparta.whyncoming.common.response.ApiResult;
+import org.sparta.whyncoming.common.response.ResponseUtil;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -18,23 +22,24 @@ public class TestController {
 
     @Operation(summary = "헬스체크")
     @GetMapping("/db")
-    public String check() {
-        return "ok";
+    public ResponseEntity<ApiResult<String>> check() {
+        return ResponseUtil.success("ok");
     }
 
     @Operation(summary = "데이터 시드 (쿼리스트링)")
     @PostMapping("/seed")
-    public String seed(
+    public ResponseEntity<ApiResult<Test>> seed(
             @Parameter(description = "내용", example = "블라블라")
             @RequestParam String name
     ) {
-        repo.save(Test.builder().name(name).build());
-        return "seeded";
+        Test saved = repo.save(Test.builder().name(name).build());
+        return ResponseUtil.success("seeded", saved);
     }
 
     @Operation(summary = "전체 조회")
     @GetMapping
-    public List<Test> list() {
-        return repo.findAll();
+    public ResponseEntity<ApiResult<List<Test>>> list() {
+        List<Test> all = repo.findAll();
+        return ResponseUtil.success("조회 성공", all);
     }
 }
