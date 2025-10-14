680667d786347630b6b6c3fb151bf5962c8f0fb7
jamie <northwing@naver.com>
2025-10-14 14:52:42 +0900

    [REFACTOR] 로그인 Dto 불변 객체로 변경

--- WhyNComing/src/main/java/org/sparta/whyncoming/user/presentation/dto/request/LoginRequestDto.java
+++ WhyNComing/src/main/java/org/sparta/whyncoming/user/presentation/dto/request/LoginRequestDto.java
@@ -1,14 +1,19 @@
 package org.sparta.whyncoming.user.presentation.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 public class LoginRequestDto {
     private final String userId;
     private final String password;
 
-    public LoginRequestDto(String userId, String password) {
+    @JsonCreator
+    public LoginRequestDto(
+            @JsonProperty("userId") String userId,
+            @JsonProperty("password") String password) {
         this.userId = userId;
         this.password = password;
     }
